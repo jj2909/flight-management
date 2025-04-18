feef86cc7975d3018a772697ab8e509791842a34
Jenny Ju <jennyju@Jennys-MacBook-Pro.local>
2025-04-18 19:22:17 +0100

    feat: ...

--- app/models/pilots.py
+++ app/models/pilots.py
@@ -6,6 +6,6 @@ from app.models.base_model import DB
 class Pilots(DB, primary_key="pilot_id"):
     first_name: str
     last_name: str
-    base: int = field(metadata={"foreign_key": {"table": "Airports", "column": "code"}})
+    base: str = field(metadata={"foreign_key": {"table": "Airports", "column": "code"}})
     airline: str
     pilot_id: Optional[int] = None
